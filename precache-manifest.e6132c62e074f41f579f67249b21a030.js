9ef3cdbcfa8bf497cc8b136c23f701b0776f9e1a
Anthony <antcon1000@gmail.com>
2020-02-02 09:12:13 -0500

    Updates

---
+++ precache-manifest.e6132c62e074f41f579f67249b21a030.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73e580e59c11262714e0c51fd04ec220",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "dfa58dd67fcd6f95a77c",
+    "url": "/monsters/static/css/main.3f30d2aa.chunk.css"
+  },
+  {
+    "revision": "d5b7dc0e95c86de42d59",
+    "url": "/monsters/static/js/2.0b9572cb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters/static/js/2.0b9572cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfa58dd67fcd6f95a77c",
+    "url": "/monsters/static/js/main.fbb07427.chunk.js"
+  },
+  {
+    "revision": "69ad86de3903771917ee",
+    "url": "/monsters/static/js/runtime-main.5e6993dc.js"
+  }
+]);
